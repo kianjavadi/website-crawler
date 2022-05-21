55a7932a1c223c14ac7bb8408db010ef404e0929
Javadi <javadi91m@gmail.com>
2022-05-21 21:09:45 +0430

    URLDecoder.decode is used for handling white space in the filePath

--- src/test/java/com/javadi/websitecrawler/io/FileSystemContentReaderTest.java
+++ src/test/java/com/javadi/websitecrawler/io/FileSystemContentReaderTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,7 +25,7 @@ public class FileSystemContentReaderTest {
     @Test
     void shouldBeAbleToReadTheContentOfAFile() throws IOException {
         File testFile = new File(getClass().getClassLoader().getResource("test.html").getFile()).getAbsoluteFile();
-        String content = contentReader.readAsString(testFile.getAbsolutePath());
+        String content = contentReader.readAsString(URLDecoder.decode(testFile.getAbsolutePath(), StandardCharsets.UTF_8));
         assertNotNull(content);
         assertNotEquals("", content);
         System.out.println("shouldBeAbleToReadTheContentOfAFile passed");
